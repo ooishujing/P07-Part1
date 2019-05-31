00d3a0ec8bcc631990617513d2b70bc66f366e52
ooishujing <18016012@myrp.edu.sg>
2019-05-31 23:48:53 +0800

    nyfxbn

--- app/src/main/java/sg/edu/rp/soi/p07_demoarrayandarraylist/MainActivity.java
+++ app/src/main/java/sg/edu/rp/soi/p07_demoarrayandarraylist/MainActivity.java
@@ -9,9 +9,13 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvResult;
-    //String[] items;
+    TextView tvTitle;
+    TextView tvDiveder;
+    String[] items;
+    //String[] fruits;
+    //register
 
-    ArrayList<String> items2;
+    ArrayList<String> fruits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,24 +23,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvResult = findViewById(R.id.tvResult);
-
+        tvTitle = findViewById(R.id.tvTitle);
+        tvDiveder = findViewById(R.id.tvDivider);
         /*
+
+        fruits = new String[3];
+        fruits[0]="apple";
+        fruits[1] ="banana";
+        fruits[2] ="cherry";
+        String title= "Fruits";
+        String divider= "========";
+        tvTitle.setText(title);
+        tvDiveder.setText(divider);
+
+
+        String result = " ";
+        for(int i= 0; i<fruits.length; i++){
+            result += fruits[i]+"\n";
+        }
+        tvResult.setText(result);
+         */
+
+
+        //no need id for Arrays
         items = new String[3];
         items[0] = "Apple";
         items[1] = "Ball";
         items[2] = "Cat";
-        */
 
-        items2 = new ArrayList<String>();
-        items2.add("Apple");
-        items2.add("Ball");
-        items2.add("Cat");
+
+        fruits = new ArrayList<String>();
+        fruits.add("Apple");
+        fruits.add("Ball");
+        fruits.add("Cherry");
 
         String data = "";
-        for (int i = 0; i < items2.size(); i++){
-            data = data + items2.get(i) + "\n";
+        for (int i = 0; i <  fruits.size(); i++){
+            data = data + fruits.get(i) + "\n";
         }
         tvResult.setText(data);
+
 
     }
 }
