4ebf488e110a578eb119ebfcf3a23a77a6c87348
Jason Lim <jason_lim@rp.edu.sg>
2019-05-31 09:09:10 +0800

    Part 2

--- app/src/main/java/sg/edu/rp/soi/p07_demoarrayandarraylist/MainActivity.java
+++ app/src/main/java/sg/edu/rp/soi/p07_demoarrayandarraylist/MainActivity.java
@@ -4,10 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView tvResult;
-    String[] items;
+    //String[] items;
+
+    ArrayList<String> items2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +20,21 @@ public class MainActivity extends AppCompatActivity {
 
         tvResult = findViewById(R.id.tvResult);
 
+        /*
         items = new String[3];
         items[0] = "Apple";
         items[1] = "Ball";
         items[2] = "Cat";
+        */
+
+        items2 = new ArrayList<String>();
+        items2.add("Apple");
+        items2.add("Ball");
+        items2.add("Cat");
 
         String data = "";
-        for (int i = 0; i < items.length; i++){
-            data = data + items[i] + "\n";
+        for (int i = 0; i < items2.size(); i++){
+            data = data + items2.get(i) + "\n";
         }
         tvResult.setText(data);
 
